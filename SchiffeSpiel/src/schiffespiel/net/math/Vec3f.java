11e0bde08d6977555e2327806f390a0053fc9a82
juhu1705 <Fabius1705@live.de>
2019-04-18 14:46:18 +0200

    Added Vec3f class

---
+++ SchiffeSpiel/src/schiffespiel/net/math/Vec3f.java
@@ -0,0 +1,24 @@
+package schiffespiel.net.math;
+
+public class Vec3f {
+
+	public float x, y, z;
+	
+	public Vec3f()	{
+		this.x = 0.0f;
+		this.y = 0.0f;
+		this.z = 0.0f;
+	}
+	
+	public Vec3f(float f)	{
+		this.x = f;
+		this.y = f;
+		this.z = f;
+	}
+	
+	public Vec3f(float x, float y, float z)	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
