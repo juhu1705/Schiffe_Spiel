7d12f446c8260722cc0da01f3b41f27b0410b4ac
juhu1705 <Fabius1705@live.de>
2019-04-28 13:53:22 +0200

    Blau

--- SchiffeSpiel/src/schiffespiel/client/engines/launch/LaunchStatusRenderer.java
+++ SchiffeSpiel/src/schiffespiel/client/engines/launch/LaunchStatusRenderer.java
@@ -1,76 +1,87 @@
 package schiffespiel.client.engines.launch;
 
 import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
 
 import schiffespiel.client.input.Input;
 import schiffespiel.client.input.Window;
-import schiffespiel.common.util.Ref;
 
 public class LaunchStatusRenderer {
-	
+
 	private static float fps;
 	private static float framerate = 200, frameTime = 1.0f / framerate;
 	private boolean isRunning;
-	
+
 	public void run() {
 		this.isRunning = true;
-		
+
 		int frames = 0;
 		long frameCounter = 0, lastTime = System.nanoTime();
 		double unprocessedTime = 0;
-		
-		//GameLoop for Rendering
-		while(this.isRunning) {
+
+		this.init();
+
+		// GameLoop for Rendering
+		while (this.isRunning) {
 			boolean render = false;
-			
+
 			long startTime = System.nanoTime(), passedTime = startTime - lastTime;
 			lastTime = startTime;
-			
-			unprocessedTime += passedTime / (double) 1000000000; //TODO Constants.nanoseconds
+
+			unprocessedTime += passedTime / (double) 1000000000; // TODO Constants.nanoseconds
 			frameCounter += passedTime;
-			
-			while(unprocessedTime > frameTime) {
+
+			while (unprocessedTime > frameTime) {
 				render = true;
 				unprocessedTime -= frameTime;
-				
-				if(Window.getInstance().isCloseRequested()) this.stop();
-				
+
+				if (Window.getInstance().isCloseRequested())
+					this.stop();
+
 				this.update();
-				
-				if(frameCounter >= 1000000000) { //TODO Constants.nanoseconds
+
+				if (frameCounter >= 1000000000) { // TODO Constants.nanoseconds
 					fps = frames;
 					frames = 0;
 					frameCounter = 0;
 				}
 			}
-			
-			if(render) {
+
+			if (render) {
 				this.render();
 				frames++;
 			} else {
 				try {
 					Thread.sleep(10);
-				} catch(InterruptedException e) {
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 		}
 		this.close();
 	}
-	
-	public void stop()	{
+
+	public void stop() {
 		this.isRunning = false;
 	}
-	
-	private void render()	{
+
+	private void init() {
+		glClearColor(0, 0.3f, 0.9f, 1.0f);
+	}
+
+	private void render() {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		Window.getInstance().render();
 	}
-	
-	private void update()	{
+
+	private void update() {
 		Input.getInstance().update();
 	}
-	
+
 	private void close() {
 		Window.getInstance().dispose();
 		glfwTerminate();
